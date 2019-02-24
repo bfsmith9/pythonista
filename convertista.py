fe888b7df8aaff3682d7ca7407cdb67b1c1224e8
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-02-23 23:42:32 -0500

    Adding files to pythonista sandbox

---
+++ convertista.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+# converts temperature to fahrenheit or celsius
+
+def print_options():
+    print("Options: ")
+    print(" 'p' print options")
+    print(" 'c' convert from celsius")
+    print(" 'f' convert from fahrenheit")
+    print(" 'q' quit the program")
+
+def celsius_to_fahrenheit(temp):
+    converter = 9.0/5.0
+    temp = float(temp) + 32.0
+    temp = temp*converter
+    return temp
+
+def fahrenheit_to_celsius(temp):
+	converter = 5.0/9.0
+	temp = float(temp) - 32.0
+	temp = temp * converter
+	return temp
+
+choice = "p"
+temp = 0.0
+float(temp)
+while choice != "q":
+    if choice == "c":
+        temp = input("Celsius temperature:")
+        # int(temp)
+        print("Fahrenheit:", celsius_to_fahrenheit(temp))
+    elif choice == "f":
+        temp = input ("Fahrenheit temperature:")
+        print("Celsius:", fahrenheit_to_celsius(temp))
+    elif choice != "q":
+        print_options()
+    choice = input("option: ")
+                      
+                   
---
+++ sandbox.py
@@ -0,0 +1,7 @@
+#! /anaconda3/bin/python
+
+print(5*5)
+
+temp = 3
+float(temp)
+print(temp*3)
